26150e4fb5eadbe9c433dc99dc4d95fe77f1c4a9
Jesse Atay <jesse.atay@gmail.com>
2018-06-11 12:54:01 -0700

    updates, new Game class, successful evaluation of win conditions for both players

---
+++ TicTacToe/TicTacToe/Classes/Game.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace TicTacToe
+{
+	public class Game
+	{
+		public string CheckWinner(string[] userOne, string[] userTwo)
+		{
+			string userOneValues = String.Join("", userOne);
+			string userTwoValues = String.Join("", userTwo);
+			Console.WriteLine($"User One values are: {userOneValues}");
+			Console.WriteLine($"User Two values are: {userTwoValues}");
+
+			//user one wins
+			CheckOnePlayer(userOneValues, "One");
+			CheckOnePlayer(userTwoValues, "Two");
+
+			return "";
+		}
+
+		public void CheckOnePlayer(string playerInput, string whichPlayer)
+		{
+			if (playerInput.Contains("1") && playerInput.Contains("2") && playerInput.Contains("3"))
+			{
+				Console.WriteLine($"Player {whichPlayer} wins!");
+			}
+			if (playerInput.Contains("4") && playerInput.Contains("5") && playerInput.Contains("6"))
+			{
+				Console.WriteLine($"Player {whichPlayer} wins!");
+			}
+			if (playerInput.Contains("7") && playerInput.Contains("8") && playerInput.Contains("9"))
+			{
+				Console.WriteLine($"Player {whichPlayer} wins!");
+			}
+			if (playerInput.Contains("1") && playerInput.Contains("4") && playerInput.Contains("7"))
+			{
+				Console.WriteLine($"Player {whichPlayer} wins!");
+			}
+			if (playerInput.Contains("2") && playerInput.Contains("5") && playerInput.Contains("8"))
+			{
+				Console.WriteLine($"Player {whichPlayer} wins!");
+			}
+			if (playerInput.Contains("3") && playerInput.Contains("6") && playerInput.Contains("9"))
+			{
+				Console.WriteLine($"Player {whichPlayer} wins!");
+			}
+			if (playerInput.Contains("1") && playerInput.Contains("5") && playerInput.Contains("9"))
+			{
+				Console.WriteLine($"Player {whichPlayer} wins!");
+			}
+			if (playerInput.Contains("3") && playerInput.Contains("5") && playerInput.Contains("7"))
+			{
+				Console.WriteLine($"Player {whichPlayer} wins!");
+			}
+		}
+	}
+}
--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -13,13 +13,17 @@ namespace TicTacToe
 			NewGame.DrawBoard(NewGame.NewBoard);
 			Console.WriteLine("");
 			NewGame.UpdatePositions(playerOneChoices, playerTwoChoices, NewGame.NewBoard, "X", "O");
+			CheckGame.CheckWinner(playerOneChoices, playerTwoChoices);
 			
         }
 
-
-		 public static GameBoard NewGame = new GameBoard();
-		public static string[] playerOneChoices = { "3", "5" };
-		public static string[] playerTwoChoices = {"1", "7", "9" };
+		
+		public static GameBoard NewGame = new GameBoard();
+		public static Game CheckGame = new Game();
+		
+		
+		public static string[] playerOneChoices = { "2", "3", "6" };
+		public static string[] playerTwoChoices = {"1", "5", "9" };
 
 
 
